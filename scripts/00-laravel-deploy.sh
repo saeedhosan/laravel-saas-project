3d1fcebc97ecbfb4487e92b5281659e6329ceb17
Saeed Hossen <appsaeed7@gmail.com>
2024-05-11 03:31:37 +0600

    change log permission

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -4,6 +4,7 @@ composer install --working-dir=/var/www/html
 
 echo "update permissions to storage and bootstrap";
 chown -R www-data:www-data /var/www/html/bootstrap /var/www/html/storage
+chown -R www-data:www-data "/var/www/html/storage/logs/*.log"
 
 echo "generating application key..."
 php artisan key:generate
