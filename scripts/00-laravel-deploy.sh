5957bd2bceb436be7cd27303841750f5ed03270d
Saeed Hossen <appsaeed7@gmail.com>
2024-05-11 05:36:15 +0600

    clear cache file too

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 composer install --working-dir=/var/www/html
 
 echo "update permissions to storage and bootstrap";
-chown -R www-data:www-data /var/www/html/bootstrap /var/www/html/storage/.
+chown -R www-data:www-data /var/www/html/bootstrap /var/www/html/storage
 
 echo "generating application key..."
 php artisan key:generate
