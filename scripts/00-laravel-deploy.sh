c81e237d7a1be172862cc5991c0d3428958641cd
Saeed Hossen <appsaeed7@gmail.com>
2024-05-11 22:03:25 +0600

    update composer, public, scan, scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,21 +2,14 @@
 echo "Running composer"
 composer install --working-dir=/var/www/html
 
-echo "update permissions to storage and bootstrap";
-
+echo "update permissions to /var/www/html";
 chmod -R 777 /var/www/html
 
-chmod -R 777 bootstrap/cache
-chmod -R 777 storage/framework/sessions
-chmod -R 777 storage/framework/cache
-chmod -R 777 storage/framework/views
-chmod -R 777 storage/logs
+# chmod -R 777 bootstrap/cache
+# chmod -R 777 storage/framework/sessions
+# chmod -R 777 storage/framework/cache
+# chmod -R 777 storage/framework/views
+# chmod -R 777 storage/logs
 
 echo "generating application key..."
 php artisan key:generate
-
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
