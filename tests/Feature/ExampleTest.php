fc9c55c714518aff5ec0e44be860ea5941afbb7e
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-12-02 03:46:02 +0600

    test(ci/cd): Add feature dummy test for test runing

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
